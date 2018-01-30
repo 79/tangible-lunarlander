2ed8c7e4304135f44c1bf18438b69edb0bbf6538
Anthony Bui <anthonybui@gmail.com>
2018-01-30 18:40:28 -0500

    Setup rotary encoder including some extra time for release... magic numbers yay!

--- tangible-mkr1000-lunarlander.ino
+++ tangible-mkr1000-lunarlander.ino
@@ -1,11 +1,16 @@
 #include "Keyboard.h";
 
-#define DEBUG 0
-#define GET_VAR_NAME(var) #var
+#define ENCODER_DO_NOT_USE_INTERRUPTS
+#include <Encoder.h>;
 
 const int thrustPin = 4;
 const int startPin = 6;
 const int resetPin = 1;
+
+Encoder wheel(5, 3);
+long oldPosition = -999;
+unsigned long lastRotation;
+
 
 void setup() {
   Serial.begin(9600);
@@ -15,6 +20,8 @@ void setup() {
   pinMode(startPin, INPUT_PULLUP);
   pinMode(resetPin, INPUT_PULLUP);
   pinMode(thrustPin, INPUT_PULLUP);
+
+  lastRotation = millis();
 }
 
 void loop() {
@@ -22,6 +29,27 @@ void loop() {
   int startReading = digitalRead(startPin);
   int resetReading = digitalRead(resetPin);
   int thrustReading = digitalRead(thrustPin);
+  long wheelPosition = wheel.read();
+
+  if (wheelPosition != oldPosition) {
+    if (wheelPosition > oldPosition) {
+      Keyboard.press(KEY_RIGHT_ARROW);
+    } else {
+      Keyboard.press(KEY_LEFT_ARROW);
+    }
+
+    oldPosition = wheelPosition;
+  }
+
+  if (wheelPosition == oldPosition) {
+    unsigned long newRotation = millis();
+
+    if (newRotation - lastRotation > 30) {
+      Keyboard.release(KEY_LEFT_ARROW);
+      Keyboard.release(KEY_RIGHT_ARROW);
+      lastRotation = newRotation;
+    }
+  }
 
   // 2. Map sensor readings to keyboard inputs
   if (startReading == LOW) {
@@ -43,7 +71,9 @@ void loop() {
   }
 
   #ifdef DEBUG
-    Serial.println("THRUST \t RESET \t START");
+    Serial.println("WHEEL \t THRUST \t RESET \t START");
+    Serial.print(wheelPosition);
+    Serial.print(" \t ");
     Serial.print(thrustReading);
     Serial.print(" \t ");
     Serial.print(resetReading);
